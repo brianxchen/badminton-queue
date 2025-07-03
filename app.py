ecbf7a284e5f3764c3c414f6ad4c10fd461ff4d7
Brian Chen <brianchen49@gmail.com>
2025-07-03 15:47:04 -0700

    Make flask listen to all network interfaces, not just localhost

--- app.py
+++ app.py
@@ -414,4 +414,4 @@ def court_updates():
     return Response(generate(), mimetype='text/event-stream')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
